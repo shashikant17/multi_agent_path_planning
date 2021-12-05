73e7fc7aaa48b4be009364759b70a7ed1fd7a7bd
Shashi Kant <skant9025@gmail.com>
2021-12-05 22:18:05 +0530

    modified aStar.py

--- centralized/cbs/aStar.py
+++ centralized/cbs/aStar.py
@@ -52,24 +52,32 @@ def aStar(m,rows,cols,mXEnd,mYEnd):
 if __name__=='__main__':
     m=maze(5,5)
     m.CreateMaze(1,5,loadMaze='maze--2021-12-05--12-45-12.csv',theme=COLOR.dark)
-    aPath=aStar(m,rows=4,cols=3,mXEnd=1,mYEnd=5)
-    # bPath=aStar(m,rows=2,cols=2)
-    # cPath=aStar(m,rows=5,cols=1)
-    dPath=DFS(m,rows=2,cols=2,mXEnd=1,mYEnd=5)
     
+    # Agent A -> A*
     a=agent(m,x=4,y=3,footprints=False,color=COLOR.red)
+    # Agent B -> BFS
     b=agent(m,color=COLOR.yellow,footprints=False)
     # c=agent(m,x=5,y=1,color=COLOR.red)
+    # Agent D -> DFS
     d=agent(m,x=2,y=2,color=COLOR.cyan,footprints=False)
     # e=agent(m,color=COLOR.black,footprints=True)
+    
+    # Agent A -> A* path
+    aPath=aStar(m,rows=4,cols=3,mXEnd=1,mYEnd=5)
+    # Agent B -> BFS path
+    bPath=m.path
+    # cPath=aStar(m,rows=5,cols=1)
+    # Agent D -> DFS path
+    dPath=DFS(m,rows=2,cols=2,mXEnd=1,mYEnd=5)
 
-    # m.tracePath({a:aPath},kill=True,delay=3000)
-    # m.tracePath({b:m.path},kill=True,delay=3000)
+    # Trace Path by all Agents
+    m.tracePath({a:aPath},kill=True,delay=1000)
+    m.tracePath({b:bPath},kill=True,delay=1000)
     # # m.tracePath({c:cPath},kill=True,delay=3000)
-    # m.tracePath({d:dPath},kill=True,delay=3000)
+    m.tracePath({d:dPath},kill=True,delay=1000)
     # m.tracePath({e:m.path},delay=3000)
 
-    m.tracePath({a:aPath,b:m.path,d:dPath},delay=600,kill=True)
+    # m.tracePath({a:aPath,b:bPath,d:dPath},delay=100,kill=True)
 
     # al=textLabel(m,'A Star Path Length for Agent A',len(aPath)+1)
     # bl = textLabel(m,'A Star Path Length for Agent B',len(bPath)+1)
